cef05bedcb53913bb604df635612f0e1b8c5c6bb
supermassimo <massimocantugarcia@gmail.com>
2021-09-07 17:34:12 -0300

    added view pipe/console check

--- src/view.c
+++ src/view.c
@@ -1,4 +1,35 @@
+#include "include/view.h"
+#include "include/shared_mem.h"
+
+#define MAX_NUM_LEN 10
+
+void verror (const char *err) {
+    fprintf(stderr, "[view] %s", err);
+}
+void verror2 (int a) {
+    fprintf(stderr, "[view] %d\n", a);
+}
+
 int main (int argc, char *argv[]) {
     //Here comes view
+    int in = 0;
+    if (isatty(fileno(stdin))) { //caso consola
+        if (argc != 2){
+            verror ("Missing file count parameter\n");
+        } else {
+            in = atoi(argv[1]);
+        }
+    } else { //caso pipe
+        if (argc != 1){
+            verror ("Error in pipe, missing count\n");
+        } else {
+            scanf("%d", &in);
+        }
+    }
+    if (in > 0){
+
+        //el resto de las cosas
+        printf("%d\n", in);
+    }
     return 0;
 }
